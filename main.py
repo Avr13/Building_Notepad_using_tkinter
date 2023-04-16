2e5c368003153ee7bae26126ccb103edbd63d7c7
priyansh <priyanshdhanuka27@gmail.com>
2023-04-15 18:00:27 -0700

    added more features

--- main.py
+++ main.py
@@ -21,6 +21,19 @@ FileMenu = Menu(MenuBar, tearoff =0)
 MenuBar.add_cascade(label="File", menu=FileMenu)
 FileMenu.add_command(label="New", command=New)
 
+FileMenu.add_command(label="Open", command=Open)
+FileMenu.add_command(label="Save", command=Save)
+
+FileMenu.add_separator()
+FileMenu.add_command(label="Exit", command=Exit)
+
+MenuBar.add_cascade(label="Edit", menu=EditMenu)
+FileMenu.add_command(label="Cut", command=Cut)
+FileMenu.add_command(label="Copy", command=Copy)
+FileMenu.add_command(label="Paste", command=Paste)
+
+MenuBar.add_cascade(label="Help", menu=HelpMenu)
+HelpMenu=Menu(MenuBar, tearoff=0)
 
 root.config(menu=MenuBar)
 root.mainloop()
